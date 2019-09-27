f0128dfbfb877afa070f852d63ecddff0852a1b9
Igor Ieromenko <igor.ieromenko@3shape.com>
2019-09-27 15:55:08 +0300

    create route

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 import './App.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Profile from './components/Profile';
 import Dialogs from './components/Dialogs';
+import News from './components/News';
+import Music from './components/Music';
+import Settings from './components/Settings';
 
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      { /*<Profile /> */}
-      <div className="app-wrapper-content">
-        <Dialogs />
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        { /*<Profile /> */}
+        <div className="app-wrapper-content">
+        <Route path="/profile" component={Profile} exact/>  
+        <Route path="/dialogs" component={Dialogs} />
+        <Route path="/news" component={News} />
+        <Route path="/music" component={Music} />
+        <Route path="/settings" component={Settings} />  
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
