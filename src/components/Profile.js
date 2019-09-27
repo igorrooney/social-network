d4870bba91df699058ee6f4fde55b8b486edf0f4
Igor Ieromenko <igor.ieromenko@3shape.com>
2019-09-27 10:44:42 +0300

    initial commit

---
+++ src/components/Profile.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+  return(
+    <div className='content'>
+      <div>
+        <img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""/>
+      </div>
+      <div>
+        ava+description
+      </div>
+      <div>
+        My posts
+        <div>
+          New post
+        </div>
+        <div>
+          <div>post1</div>
+          <div>post2</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
