31a79d1efc2245ba25300cf4877b3f6e53718672
Igor Ieromenko <igor.ieromenko@3shape.com>
2020-04-17 15:54:40 +0300

    refactor app-reducer

--- src/redux/app-reducer.ts
+++ src/redux/app-reducer.ts
@@ -1,10 +1,7 @@
+import { Dispatch } from 'redux'
 import { authMe } from './auth-reducer'
 import { ThunkAction } from 'redux-thunk'
-import { AppStateType } from './redux-store'
-import { Dispatch } from 'redux'
-
-const INITIAL_SUCCESS = '/app/INITIAL_SUCCESS'
-const SET_ERROR = '/app/SET_ERROR'
+import { AppStateType, InfernActionsTypes } from './redux-store'
 
 const initialState = {
   initialized: false,
@@ -15,13 +12,13 @@ export type InitialStateType = typeof initialState
 
 const appReducer = (state = initialState, action: ActionsType): InitialStateType => {
   switch (action.type) {
-    case INITIAL_SUCCESS:
+    case "INITIAL_SUCCESS":
       return {
         ...state,
         initialized: true
       }
 
-    case SET_ERROR:
+    case "SET_ERROR":
       return {
         ...state,
         globalError: action.globalError
@@ -32,35 +29,29 @@ const appReducer = (state = initialState, action: ActionsType): InitialStateType
   }
 }
 
-type ActionsType = InitialSuccessActionType | SetErrorActionType
+type ActionsType = InfernActionsTypes<typeof actions>
 
-type InitialSuccessActionType = {
-  type: typeof INITIAL_SUCCESS
+export const actions = {
+  initialSuccess: () => ({ type: 'INITIAL_SUCCESS' } as const),
+  setError: (globalError: any) => ({ type: 'SET_ERROR', globalError } as const)
 }
-const initialSuccess = (): InitialSuccessActionType => ({ type: INITIAL_SUCCESS })
-
-type SetErrorActionType = {
-  type: typeof SET_ERROR
-  globalError: any // need to check!
-}
-const setError = (globalError: any): SetErrorActionType => ({ type: SET_ERROR, globalError })
 
 type DispatchType = Dispatch<ActionsType>
 
-export const initializing = ()
-  : ThunkAction<void, AppStateType, unknown, ActionsType> => {
+export const initializing = ():
+  ThunkAction<void, AppStateType, unknown, ActionsType> => {
   return (dispatch) => {
     const auth = dispatch(authMe())
     Promise.all([auth]).then(() => {
-      dispatch(initialSuccess())
+      dispatch(actions.initialSuccess())
     })
   }
 }
 
 export const saveError = (error: any) => {
   return (dispatch: DispatchType) => {
-    dispatch(setError(error))
-    setTimeout(() => dispatch(setError(null)), 3000)
+    dispatch(actions.setError(error))
+    setTimeout(() => dispatch(actions.setError(null)), 3000)
   }
 }
 
